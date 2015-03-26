f0dd31bc285bda27435fa70734507b1d1703d460
Jack Dipper <jack@jackdipper.com>
2015-03-26 00:27:52 +0000

    Minor bug fixes

--- src/Webb/FileBundle/Form/Type/ImageType.php
+++ src/Webb/FileBundle/Form/Type/ImageType.php
@@ -11,7 +11,6 @@ class ImageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('file');
-        //$builder->add('path');
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
